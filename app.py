69bc1e6680f8551c97edab50fe834471bc8adfe8
fabio1236791 <olkesandrshevchenko889@gmail.com>
2020-01-13 12:41:38 +0200

    first commit

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     content = db.Column(db.String(200), nullable = False)
     date_created = db.Column(db.DateTime, default= datetime.utcnow)
+    comment = db.Column(db.String(200), nullable = False)
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -19,7 +20,7 @@ class Todo(db.Model):
 def index():
     if request.method == 'POST':
         task_content = request.form['content']
-        new_task = Todo(content = task_content)
+        new_task = Todo(content = task_content, comment = 'None')
         try:
             db.session.add(new_task)
             db.session.commit()
@@ -41,6 +42,8 @@ def delete(id):
     except:
         return "We have mutual problem with deleting the task"
 
+
+
 @app.route('/update/<int:id>', methods = ['GET', 'POST'])
 def update(id):
     task = Todo.query.get_or_404(id)
@@ -53,5 +56,19 @@ def update(id):
             return "You have major problem with updating your task"
     else:
         return render_template('update.html', task = task)
+
+@app.route('/comment/<int:id>', methods = ['GET', 'POST'])
+def comment(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.comment = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "You have major problem with commenting your task"
+    else:
+        return render_template('comment.html', task = task)
+
 if __name__ == "__main__":
     app.run(debug=True)
